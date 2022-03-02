e2bd82c9e1ce8bf6c42b121b6e3ec25414bed49c
Glenda Teo <glenda@sgcodecampus.com>
2022-03-02 13:15:34 +0800

    Initial Commit

---
+++ Elderberry-senior-app WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Elderberry-senior-app WatchKit Extension
+//
+//  Created by glendatxn on 2/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
